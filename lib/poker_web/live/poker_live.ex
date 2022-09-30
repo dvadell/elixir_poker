7e5d6c261ab6fe12579c84183a3a24942c7454d7
Diego Vadell <diego.m.vadell@gmail.com>
2022-09-30 19:25:11 +0000

    timer

--- lib/poker_web/live/poker_live.ex
+++ lib/poker_web/live/poker_live.ex
@@ -2,7 +2,20 @@ defmodule PokerWeb.PokerLive do
   use PokerWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, vote: 0, name: "Diego", topic: "No topic defined") # Valor inicial
+    socket = 
+      if connected?(socket) do
+        {:ok, tref} = :timer.send_interval(1000, self(), :tick)
+        assign(socket, tref: tref)
+      else
+        socket
+      end
+
+    expiration_timex = Timex.shift(Timex.now(), minutes: 1)
+    IO.inspect(expiration_timex)
+
+    socket = assign(socket, vote: 0, name: "Diego", topic: "No topic defined", 
+                            expiration_timex: expiration_timex,
+                            time_remaining: time_remaining(expiration_timex) ) # Valor inicial
     {:ok, socket}
   end
 
@@ -19,6 +32,14 @@ defmodule PokerWeb.PokerLive do
     <%= @vote %>
 
     <button phx-click="vote"> Vote </button>
+
+    <p class="m-4 font-semibold text-indigo-800">
+      <%= if @time_remaining > 0 do %>
+        <%= format_time(@time_remaining) %> 
+      <% else %>
+        Expired!
+      <% end %>
+    </p>
     """
   end
 
@@ -33,5 +54,31 @@ defmodule PokerWeb.PokerLive do
   def handle_event("update", %{"name" => name, "topic" => topic}, socket) do
     socket = assign(socket, topic: topic, name: name)
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    expiration_timex = socket.assigns.expiration_timex
+    IO.inspect(expiration_timex, label: "Expiration_time")
+    socket = 
+      if time_remaining(expiration_timex) < 1 do
+        :timer.cancel(socket.assigns.tref)
+        assign(socket, tref: nil)
+      else
+        socket
+      end
+    socket = assign(socket, time_remaining: time_remaining(expiration_timex))
+    {:noreply, socket}
+  end
+
+  # Helper functions
+  ##################
+  defp time_remaining(expiration_timex) do
+    DateTime.diff(expiration_timex, Timex.now())
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end
